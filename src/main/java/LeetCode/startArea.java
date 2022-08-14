34cbc2cc200f3c5d977f281309b12623d68d5e28
kainthekiller <phillipyates12@hotmail.com>
2022-08-14 14:48:19 -0500

    Palindrome Completed

--- src/main/java/LeetCode/startArea.java
+++ src/main/java/LeetCode/startArea.java
@@ -3,9 +3,8 @@ package LeetCode;
 public class startArea {
 
     public static void main(String[] args) {
-        Palindrome palindrome = new Palindrome();
-        int testCase = 1521212551;
-        palindrome.isPalindrome(555);
+        //Palindrome *Completed
+
 
 
     }
